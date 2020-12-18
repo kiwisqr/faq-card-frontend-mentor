8b1ddb95e7d684e56dc8115cd8338268ee338f2a
kiwisqr <75386663+kiwisqr@users.noreply.github.com>
2020-12-18 23:03:59 +0200

    final

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ for (i = 0; i < acc.length; i++) {
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
+      let active = document.querySelectorAll(".second-half-box .second-half-accordion.active");
+      for(let j = 0; j < active.length; j++){
+        active[j].classList.remove("active");
+        active[j].nextElementSibling.style.maxHeight = null;
+      }
+      this.classList.toggle("active");
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+}
+});
 }
